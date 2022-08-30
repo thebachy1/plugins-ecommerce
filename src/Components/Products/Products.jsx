e50fc04fa08b18122a65629517d3698df28bd1b3
Manuel <manuelcu2299@hotmail.com>
2022-08-30 09:20:56 -0500

    changes on products component

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Products.scss";
+import bannerImage from "../../assets/logo.svg";
+import productsMock from "../../Mocks/Products.json";
 
 export default function Products() {
-  return <div>Products</div>;
+  const [products, setProducts] = useState(productsMock);
+
+  return (
+    <section className="products">
+      {products &&
+        products.map((product) => (
+          <div className="product-card" key={product.id}>
+            <div className="product-image">
+              <img src={product.image} />
+            </div>
+            <div className="product-info">
+              <h5>{product.name}</h5>
+              <h6>{product.price}</h6>
+              <button class="button-74">Go to product</button>
+            </div>
+          </div>
+        ))}
+    </section>
+  );
 }
